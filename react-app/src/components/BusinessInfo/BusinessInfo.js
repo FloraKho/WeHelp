d64e6ff4e617c77a7b5bc0564e1f7ab4b7eb9336
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-07-25 10:49:47 -0700

    render info

--- react-app/src/components/BusinessInfo/BusinessInfo.js
+++ react-app/src/components/BusinessInfo/BusinessInfo.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { getReviewThunk } from '../../store/reviews';
 import { getBusinessThunk } from '../../store/businesses'
 import { getBizImagesThunk } from '../../store/images'
+import './BusinessInfo.css'
 
 
 
@@ -16,6 +17,7 @@ function BusinessInfo() {
     const reviews = useSelector(state => state.reviewState);
     const images = useSelector (state => state.imageState);
     const businessInfo = useSelector (state => state.bizState);
+    const singleBusiness = businessInfo[parseInt(businessId)];
 
     const [isLoaded, setLoaded] = useState(false);
 
@@ -25,15 +27,24 @@ function BusinessInfo() {
         .then(()=>dispatch(getBizImagesThunk(parseInt(businessId))))
         .then(()=>setLoaded(true))
     },[])
-    console.log(reviews);
-    console.log(images);
-    console.log(businessInfo);
+    console.log(Object.values(images));
 
     return(
         isLoaded&&
         <div>
-            <h1>BusinessInfo for ID {businessId}</h1>
-            <div>{businessInfo[parseInt(businessId)].name }</div>
+            {Object.values(images).map(({ id, image_url }) => (
+                <div className='background' key={id}>
+                    <div className="image_container" style={{backgroundImage:`url(${image_url})`}}></div>
+                </div>
+            ))}
+            <h1>{singleBusiness?.name }</h1>
+            <p><span>· {singleBusiness.price_range} ·</span>{singleBusiness?.description}</p>
+            <h2>Location & Hours</h2>
+            <p>Operation Hours: {singleBusiness?.business_hours}</p>
+            <p>Location: {singleBusiness?.address}, {singleBusiness?.city}, {singleBusiness.zip_code} </p>
+            <p>Phone: {singleBusiness.phone}</p>
+            <p>Website: {singleBusiness.website}</p>
+            <h2>Recommened Reviews</h2>
         </div>
     )
 }
