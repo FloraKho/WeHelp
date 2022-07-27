e3a08f9dc074488a3cbdbcdcb8c82a0eeb116cdf
tshao42 <tshao0602@gmail.com>
2022-07-26 23:18:28 -0700

    single map working; search map IP

---
+++ react-app/src/components/SingleMapContainer/SingleMapContainer.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const SingleMapContainer = ({latitude, longitude}) => {
+
+    console.log(`line 6 ${latitude} ${longitude}`)
+    const mapStyles = {
+        height: "30vh",
+        width: "50%"
+    };
+
+    const defaultCenter = {
+        lat: latitude, lng: longitude
+    }
+
+
+    const pin = {
+        lat: latitude, lng: longitude
+    }
+
+    return (
+        <div>
+            <GoogleMap
+                mapContainerStyle={mapStyles}
+                zoom={16}
+                center={defaultCenter}
+            >
+                
+                <Marker 
+                position={pin}
+                />
+            </GoogleMap>
+            
+        </div>
+    )
+}
+
+export default SingleMapContainer;
