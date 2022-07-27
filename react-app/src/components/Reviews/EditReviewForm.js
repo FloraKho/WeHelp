d50e090c29c3fcd827d6da49679d0230450c7e5a
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-07-27 13:23:59 -0700

    review post/update css done

--- react-app/src/components/Reviews/EditReviewForm.js
+++ react-app/src/components/Reviews/EditReviewForm.js
@@ -4,13 +4,14 @@ import { useSelector, useDispatch } from "react-redux";
 import { getBusinessThunk } from '../../store/businesses'
 import { Redirect } from 'react-router-dom';
 import { getSingleReviewThunk, updateReviewThunk } from "../../store/reviews";
+import "./Reviews.css"
+import { BsFillBookmarkStarFill } from "react-icons/bs";
 
 const EditReviewForm = () => {
     const dispatch = useDispatch();
     const history = useHistory();
     const { currentReviewId } = useParams();
-    console.log(`current Id: ${currentReviewId}`)
-    
+
     const review = useSelector(state => state.reviewState);
     useEffect(() => {
         dispatch(getSingleReviewThunk(parseInt(currentReviewId)));
@@ -20,9 +21,27 @@ const EditReviewForm = () => {
     const currentReview = review[parseInt(currentReviewId)];
     const [newRating, setRating] = useState(currentReview?.rating)
     const [newContent, setContent] = useState(currentReview?.content);
-    
+    const stars = Array(5).fill(0)
+    const [hoverVal, setHoverVal] = useState(undefined);
     const businessId = currentReview?.business_id;
 
+    const colors = {
+        red: "#FF0033",
+        grey: "#a9a9a9"
+
+    };
+
+    const handleClick = value => {
+        setRating(value)
+    }
+
+    const handleMouseOver = newHoverValue => {
+        setHoverVal(newHoverValue)
+    };
+
+    const handleMouseLeave = () => {
+        setHoverVal(undefined)
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -42,29 +61,42 @@ const EditReviewForm = () => {
     //
     return (
         <div>
-        {currentReview &&
-            <form onSubmit={handleSubmit}>
-                <label>Rating</label>
-                <input
-                    name='rating'
-                    type='number'
-                    value={newRating}
-                    required={true}
-                    onChange={(e) => setRating(e.target.value)}
-                />
-                <label>Content</label>
-                <input
-                    name="content"
-                    type="text"
-                    value={newContent}
-                    onChange={(e) => setContent(e.target.value)}
-                    required={true}
-                />
-                <button type="submit">Post Review</button>
-            </form>
-        }       
+            {currentReview &&
+                <div className="review-page">
+                    <h1>Update Your Review</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="review-form">
+                            <div id="rating-stars">
+                                {stars.map((_, index) => {
+                                    return (
+                                        <BsFillBookmarkStarFill
+                                            key={index}
+                                            value={newRating}
+                                            size={40}
+                                            onClick={() => handleClick(index + 1)}
+                                            onMouseOver={() => handleMouseOver(index + 1)}
+                                            onMouseLeave={handleMouseLeave}
+                                            onChange={(e) => setRating(e.target.value)}
+                                            color={(hoverVal || newRating) > index ? colors.red : colors.grey}
+                                        />
+                                    )
+                                })}
+                            </div>
+                            <textarea
+                                className="review-textarea"
+                                name="content"
+                                type="text"
+                                value={newContent}
+                                onChange={(e) => setContent(e.target.value)}
+                                required={true}
+                            />
+                        </div>
+                        <button className="post-review-button" type="submit">Post Review</button>
+                    </form>
+                </div>
+            }
         </div>
-        
+
     )
 }
 
--- react-app/src/components/Reviews/ReviewForm.js
+++ react-app/src/components/Reviews/ReviewForm.js
@@ -1,26 +1,50 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
 import { getBusinessThunk } from '../../store/businesses'
 import { Redirect } from 'react-router-dom';
 import { getReviewThunk, addReviewThunk } from "../../store/reviews";
+import "./Reviews.css"
+import { BsFillBookmarkStarFill } from "react-icons/bs";
 
 const ReviewForm = () => {
     const dispatch = useDispatch();
     const history = useHistory();
-    const {businessId} = useParams();
-    useEffect (()=>{
+    const { businessId } = useParams();
+
+    useEffect(() => {
         dispatch(getBusinessThunk(parseInt(businessId)))
-    },[])
-    const businessInfo = useSelector (state => state.bizState);
+    }, [])
+
+    const businessInfo = useSelector(state => state.bizState);
     const singleBusiness = businessInfo[parseInt(businessId)];
+    const stars = Array(5).fill(0)
 
 
     const [errors, setErrors] = useState([]);
     const [rating, setRating] = useState(0);
     const [content, setContent] = useState('');
+    const [hoverVal, setHoverVal] = useState(undefined);
     const user = useSelector(state => state.session.user)
-  
+
+    const colors = {
+        red: "#FF0033",
+        grey: "#a9a9a9"
+
+    };
+
+    const handleClick = value => {
+        setRating(value)
+    }
+
+    const handleMouseOver = newHoverValue => {
+        setHoverVal(newHoverValue)
+    };
+
+    const handleMouseLeave = () => {
+        setHoverVal(undefined)
+    }
+
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -30,37 +54,46 @@ const ReviewForm = () => {
             rating: rating,
             content: content
         })
-        await 
-        dispatch(addReviewThunk(payload))
-        .then(()=>dispatch(getReviewThunk(payload)));
+        await
+            dispatch(addReviewThunk(payload))
+                .then(() => dispatch(getReviewThunk(payload)));
 
         history.push(`/businesses/${businessId}`);
-           
     }
 
     //
     return (
-        <div>
+        <div className="review-page">
             <h1>{singleBusiness?.name}</h1>
             <form onSubmit={handleSubmit}>
-                <label>Rating</label>
-                <input
-                    name='rating'
-                    type='number'
-                    value={rating}
-                    required = {true}
-                    onChange={(e) => setRating(e.target.value)}
-                />
-                <label>Content</label>
-                <input
-                    name="content"
-                    type="text"
-                    value={content}
-                    onChange={(e) => setContent(e.target.value)}
-                    placeholder="Please leave your review here"
-                    required={true}
-                />
-                <button type="submit">Post Review</button>
+                <div className="review-form">
+                    <div id="rating-stars">
+                        {stars.map((_, index) => {
+                            return (
+                                <BsFillBookmarkStarFill
+                                    key={index}
+                                    value={rating}
+                                    size={40}
+                                    onClick={() => handleClick(index + 1)}
+                                    onMouseOver={() => handleMouseOver(index + 1)}
+                                    onMouseLeave={handleMouseLeave}
+                                    onChange={(e) => setRating(e.target.value)}
+                                    color={(hoverVal || rating) > index ? colors.red : colors.grey}
+                                />
+                            )
+                        })}
+                    </div>
+                    <textarea
+                        className="review-textarea"
+                        name="content"
+                        type="text"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        placeholder="Came here on a Saturday night spontaneously- I've heard about this place many times before and was excited when they were able to seat us right away upon arrival. I didn't have expectations for this place, but with each and every dish was blown away by the complexity of flavors and attention to detail."
+                        required={true}
+                    />
+                </div>
+                <button className="post-review-button" type="submit">Post Review</button>
             </form>
         </div>
     )
