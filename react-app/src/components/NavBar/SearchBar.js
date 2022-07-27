ef2252fa348b288b05fc7a4aa66937f17f3af25c
jiaqicheng1998 <jiaqicheng1998@gmail.com>
2022-07-26 18:19:57 -0700

    nav bar done

--- react-app/src/components/NavBar/SearchBar.js
+++ react-app/src/components/NavBar/SearchBar.js
@@ -14,7 +14,7 @@ const SearchBar = ({ businesses }) => {
 
     return (
         <>
-            <div>
+            <div id="search-input-button">
                 <input
                     className="search-bar-input"
                     type="text"
@@ -22,29 +22,32 @@ const SearchBar = ({ businesses }) => {
                     value={query}
                     onChange={event => {
                         setShowMenu(true)
-                        setQuery(event.target.value)}}
-                    />
-                <i className="fa-solid fa-magnifying-glass fa-2x glass" onClick={handleSearch} />
-            { showMenu && 
-                <div className="search-bar-drop-down-menu">
-                    {query && businessesArr.filter(business => {
-                        if (query === "") {
-                            //if query is empty
-                            return business;
-                        } else if (business.name.toLowerCase().includes(query.toLowerCase())) {
-                            //returns filtered array
-                            return business;
-                        }
-                    }).map((business, index) => (
-                        <NavLink to={`/businesses/${business.id}`} onClick={() => setQuery("")}>
-                            <div key={index}>
-                                <p>{business.name}</p>
-                            </div>
-                        </NavLink>
-                    )
-                    )
-                    }
+                        setQuery(event.target.value)
+                    }}
+                />
+                <div id="search-button-container">
+                    <i id="search-button" className="fa-solid fa-magnifying-glass fa-lg glass" onClick={handleSearch} />
                 </div>
+                {showMenu &&
+                    <div className="search-bar-drop-down-menu">
+                        {query && businessesArr.filter(business => {
+                            if (query === "") {
+                                //if query is empty
+                                return business;
+                            } else if (business.name.toLowerCase().includes(query.toLowerCase())) {
+                                //returns filtered array
+                                return business;
+                            }
+                        }).map((business, index) => (
+                            <NavLink to={`/businesses/${business.id}`} onClick={() => setQuery("")}>
+                                <div key={index}>
+                                    <p>{business.name}</p>
+                                </div>
+                            </NavLink>
+                        )
+                        )
+                        }
+                    </div>
                 }
             </div>
         </>
